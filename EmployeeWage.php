9fdaf83f3d48ea3a7311dfbe364614cdcb45e526
samruddhichitnis02 <samruddhichitnis02@gmail.com>
2019-11-19 16:24:35 +0530

    daily employee wage calculated

--- EmployeeWage.php
+++ EmployeeWage.php
@@ -5,12 +5,13 @@ class Employee
     public $employee_name;
     public function __construct()
     {
-        $this->employee_name="";
+        $this->employee_name=null;
     }
     public function employee_Attendance()
     {
         echo "Enter the name of employee:";
         fscanf(STDIN,"%s",$this->employee_name);
+        //random function 1 for present 0 for absent
         $attendance=rand(0,1);
         if($attendance==1)
         {
@@ -21,8 +22,18 @@ class Employee
             echo $this->employee_name." is absent\n";
         }
     }
+    public function daily_EmployeeWage()
+    {
+        echo "Enter the name of employee:";
+        fscanf(STDIN,"%s",$this->employee_name);
+        $wage_per_hour=20;
+        $full_day_hour=8;
+        //calculate daily wage of employee
+        $wage_per_day=$wage_per_hour*$full_day_hour;
+        echo "Daily Wages of ".$this->employee_name." is:".$wage_per_day."\n";
+    }
 }
 $object=new Employee;
-$object->employee_Attendance();
+$object->daily_EmployeeWage();
 
 ?>
