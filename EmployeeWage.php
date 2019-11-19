1156220318581afd0ecfbe301b140fd3559d70d4
Sushil <SushilMishra0712>
2019-11-19 17:17:13 +0530

    modified program and added part time employee wages

--- EmployeeWage.php
+++ EmployeeWage.php
@@ -20,22 +20,27 @@ class Employee
         echo "Enter the name of employee:";
         fscanf(STDIN,"%s",$this->employee_name);
         //random function 1 for present 0 for absent
-        $attendance=rand(0,1);
-        if($attendance==1)
+        $attendance=rand(0,2);
+        if($attendance==0)
         {
-            echo $this->employee_name." is present\n";
+            echo $this->employee_name." is Absent\n";
         }
-        else
+        else if($attendance==1)
         {
-            echo $this->employee_name." is absent\n";
+            echo $this->employee_name." is Present\n";
+            echo $this->employee_name." is Full-time employee\n";
+            self::daily_EmployeeWage();
+        }
+        else 
+        {
+            echo $this->employee_name." is Present\n";
+            echo $this->employee_name." is Part-time employee\n";
+            self::partTime_EmployeeWage();
         }
     }
 
     public function daily_EmployeeWage()
     {
-        echo "Enter the name of employee:";
-        fscanf(STDIN,"%s",$this->employee_name);
-       
         //calculate daily wage of employee
         $wages_per_day = $this->wage_per_hour*$this->full_day_hour;
         echo "Daily Wages of ".$this->employee_name." is:".$wages_per_day."\n";
@@ -43,15 +48,12 @@ class Employee
 
     public function partTime_EmployeeWage()
     {
-        echo "Enter name of part time employee:";
-        fscanf(STDIN,"%s",$this->employee_name);
-
         //calculate daily wage of part time employee
         $wages_per_day = $this->wage_per_hour*$this->part_time_hour;
         echo "Daily Wages of ".$this->employee_name." is:".$wages_per_day."\n";
     }
 }
 $object=new Employee;
-$object->partTime_EmployeeWage();
+$object->employee_Attendance();
 
 ?>
