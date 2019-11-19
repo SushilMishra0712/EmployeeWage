618a8be81929857da4dfb2f48f5aa505d8611c55
Sushil <SushilMishra0712>
2019-11-19 19:30:04 +0530

    stored daily wage along with total wage

--- EmployeeWage.php
+++ EmployeeWage.php
@@ -24,7 +24,7 @@ class Employee
         echo "Enter the name of employee:";
         fscanf(STDIN,"%s",$this->employee_name);
         //random function 1 for present 0 for absent
-        $attendance=rand(0,2);
+        $attendance=rand(0,3);
         switch($attendance)
         {
             case 0:
@@ -39,6 +39,10 @@ class Employee
                 echo $this->employee_name." is Present\n";
                 echo $this->employee_name." is Part-time employee\n";
                 self::Monthly_Parttime_EmployeeWage();
+            break;
+            case 3:
+                echo "Store daily wages along with total wage:\n";
+                self::Store_daily_Wage();
             break;
         }
     }
@@ -57,7 +61,7 @@ class Employee
         }
         else
         {
-            $wages_per_month = $this->max_working_hour_permonth*$this->max_working_day_permonth*$this->wage_per_hour;
+            $wages_per_month = $this->max_working_hour_permonth*$this->wage_per_hour;
             echo "Monthly Wages of ".$this->employee_name." is:".$wages_per_month."\n";
         }
     }
@@ -76,9 +80,35 @@ class Employee
         }
         else
         {
-            $wages_per_month = $this->max_working_hour_permonth*$this->max_working_day_permonth*$this->wage_per_hour;
+            $wages_per_month = $this->max_working_hour_permonth*$this->wage_per_hour;
             echo "Monthly Wages of ".$this->employee_name." is:".$wages_per_month."\n";
         }
+    }
+
+    public function Store_daily_Wage()
+    {
+        $total_wage=0;
+        for($i=1;$i<21;$i++)
+        {
+            echo "How many hours do you work on day ".$i." ?\n";
+            fscanf(STDIN,"%d",$this->full_day_hour);
+            if($this->full_day_hour>8)
+            {
+                echo "Maximum working hour per day is 8..Enter again\n";
+                $i--;
+                continue;
+            }
+            $wage_per_day=$this->full_day_hour*$this->wage_per_hour;
+            $total_wage=$total_wage+$wage_per_day;
+            if($total_wage>2000)
+            {
+                print_r($temp_array);
+                echo "You have reached your working hour limit\n";
+                exit(0);
+            }
+            $temp_array[$i]=array($wage_per_day,$total_wage);
+        }
+        print_r($temp_array);
     }
 }
 
